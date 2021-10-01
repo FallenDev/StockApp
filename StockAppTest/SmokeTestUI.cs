63509b390eccdaac99ea42583f63ad8d9c2fa65a
Buck, Paul R <paul.buck86@gmail.com>
2021-10-01 14:23:05 -0400

    Integration Tests

--- StockAppTest/SmokeTestUI.cs
+++ StockAppTest/SmokeTestUI.cs
@@ -1,66 +1,139 @@
-//using System;
-//using System.Diagnostics;
-//using System.Drawing;
-//using System.IO;
-//using System.Linq;
-//using System.Reflection;
-//using System.Threading;
-//using System.Windows.Media.Imaging;
-//using FlaUI.Core;
-//using FlaUI.Core.AutomationElements;
-//using FlaUI.Core.Input;
-//using FlaUI.TestUtilities;
-//using FlaUI.UIA3;
-//using NUnit.Framework;
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using System.Windows.Automation;
+using System.Windows.Media.Imaging;
+using FlaUI.Core;
+using FlaUI.Core.AutomationElements;
+using FlaUI.Core.Conditions;
+using FlaUI.Core.Input;
+using FlaUI.Core.Tools;
+using FlaUI.UIA3;
+using MahApps.Metro.Controls;
+using NUnit.Framework;
 
-//using StockApp;
+using StockApp;
+using AutomationElement = FlaUI.Core.AutomationElements.AutomationElement;
 
-//namespace StockAppTest
-//{
-//    [TestFixture]
-//    public class SmokeTestUI : FlaUITestBase
-//    {
-//        private Window _window;
-//        private Stopwatch _stop;
+namespace StockAppTest
+{
+    [TestFixture]
+    public class SmokeTestUI
+    {
+        private Window _window;
+        private Stopwatch _stop;
+        private Application _app;
+        private ConditionFactory _cF;
+        private static readonly TimeSpan DefaultTimeout = TimeSpan.FromSeconds(1);
 
-//        protected override AutomationBase GetAutomation()
-//        {
-//            return new UIA3Automation();
-//        }
+        [SetUp]
+        public void Setup()
+        {
+            _stop = Stopwatch.StartNew();
+            _app = Application.Launch("StockApp.exe");
+            using var auto = new UIA3Automation();
+            _window = _app.GetMainWindow(auto);
+            _cF = new ConditionFactory(new UIA3PropertyLibrary());
+        }
 
-//        protected override Application StartApplication()
-//        {
-//            var app = Application.Launch("StockApp.exe");
-//            app.WaitWhileMainHandleIsMissing();
-//            return app;
-//        }
+        [TearDown]
+        public void TearDown()
+        {
+            _stop.Stop();
+            _app.Close();
+        }
 
-//        [SetUp]
-//        public void Setup()
-//        {
-//            _stop = Stopwatch.StartNew();
-//            Mouse.Position = new Point(0, 0);
-//        }
+        [Test]
+        public void TC1_1_StartApp()
+        {
+            Assert.That(_app, Is.Not.Null);
+        }
 
-//        [TearDown]
-//        public void TearDown()
-//        {
-//            _stop.Stop();
-//            Console.WriteLine("Time elapsed: " + _stop.ElapsedMilliseconds);
-//        }
+        [Test]
+        public void TC1_2_MainWindowUI()
+        {            
+            Assert.That(_window, Is.Not.Null);
+        }
 
-//        [OneTimeTearDown]
-//        public void FixtureTeardown()
-//        {
-//            Automation?.Dispose();
-//            Application?.Close();
-//        }
+        [Test]
+        public void TC1_3_StockScreenerUI()
+        {
+            var stockTab = _window.FindFirstDescendant(_cF.ByText("Stock Screener")).AsTabItem().Select();
+            stockTab.Click();
+            Assert.That(stockTab.IsSelected);
+        }
 
-//        [Test]
-//        public void SmokeTest_MainWindowUI()
-//        {
-//            _window = Application.GetMainWindow(Automation);
-//            Assert.That(_window, Is.Not.Null);
-//        }
-//    }
-//}
+        [Test]
+        public void TC1_4_StockScreenerDisplayTopResultsNasdaq100()
+        {
+            var stockTab = _window.FindFirstDescendant(_cF.ByText("Stock Screener")).AsTabItem().Select();
+            stockTab.Click();
+            _window.FindFirstDescendant(_cF.ByAutomationId("MoversGrp")).AsComboBox().Select(1).Click();
+
+            var stockData = _window.FindFirstDescendant(_cF.ByAutomationId("MoversList")).AsListBox().Items;
+            Assert.That(stockData, Is.Not.Null);
+        }
+
+        [Test]
+        public void TC1_5_StockScreenerDisplayBottomResultsNasdaq100()
+        {
+            var stockTab = _window.FindFirstDescendant(_cF.ByText("Stock Screener")).AsTabItem().Select();
+            stockTab.Click();
+            _window.FindFirstDescendant(_cF.ByAutomationId("OnToggle")).AsToggleButton().Click();
+            _window.FindFirstDescendant(_cF.ByAutomationId("MoversGrp")).AsComboBox().Select(1).Click();
+
+            var stockData = _window.FindFirstDescendant(_cF.ByAutomationId("MoversList")).AsListBox().Items;
+            Assert.That(stockData, Is.Not.Null);
+        }
+
+        [Test]
+        public void TC1_6_StockScreenerDisplayTopResultsDow30()
+        {
+            var stockTab = _window.FindFirstDescendant(_cF.ByText("Stock Screener")).AsTabItem().Select();
+            stockTab.Click();
+            _window.FindFirstDescendant(_cF.ByAutomationId("MoversGrp")).AsComboBox().Select(0).Click();
+
+            var stockData = _window.FindFirstDescendant(_cF.ByAutomationId("MoversList")).AsListBox().Items;
+            Assert.That(stockData, Is.Not.Null);
+        }
+
+        [Test]
+        public void TC1_7_StockScreenerDisplayBottomResultsDow30()
+        {
+            var stockTab = _window.FindFirstDescendant(_cF.ByText("Stock Screener")).AsTabItem().Select();
+            stockTab.Click();
+            _window.FindFirstDescendant(_cF.ByAutomationId("OnToggle")).AsToggleButton().Click();
+            _window.FindFirstDescendant(_cF.ByAutomationId("MoversGrp")).AsComboBox().Select(0).Click();
+
+            var stockData = _window.FindFirstDescendant(_cF.ByAutomationId("MoversList")).AsListBox().Items;
+            Assert.That(stockData, Is.Not.Null);
+        }
+
+        [Test]
+        public void TC1_8_StockScreenerDisplayTopResultsSP500()
+        {
+            var stockTab = _window.FindFirstDescendant(_cF.ByText("Stock Screener")).AsTabItem().Select();
+            stockTab.Click();
+            _window.FindFirstDescendant(_cF.ByAutomationId("MoversGrp")).AsComboBox().Select(2).Click();
+
+            var stockData = _window.FindFirstDescendant(_cF.ByAutomationId("MoversList")).AsListBox().Items;
+            Assert.That(stockData, Is.Not.Null);
+        }
+
+        [Test]
+        public void TC1_9_StockScreenerDisplayBottomResultsSP500()
+        {
+            var stockTab = _window.FindFirstDescendant(_cF.ByText("Stock Screener")).AsTabItem().Select();
+            stockTab.Click();
+            _window.FindFirstDescendant(_cF.ByAutomationId("OnToggle")).AsToggleButton().Click();
+            _window.FindFirstDescendant(_cF.ByAutomationId("MoversGrp")).AsComboBox().Select(2).Click();
+
+            var stockData = _window.FindFirstDescendant(_cF.ByAutomationId("MoversList")).AsListBox().Items;
+            Assert.That(stockData, Is.Not.Null);
+        }
+    }
+}
